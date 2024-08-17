cfc27f3aecfef84bf7e4b914c13da6ffa57e46c2
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-08-17 17:14:38 -0300

    Added basic handlers

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hola mundo!')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    const content = req.body.properties.Name.title[0].text.content;
+    console.log("content: ", content)
+    const splitContent = content.split('\n');
+    console.log("splitContent: ", splitContent)
+
+    // Agregar el contenido spliteado al body de respuesta
+    const responseBody = {
+      original_content: content,
+      split_content: splitContent
+    }
+    res.send(responseBody)
+})
+
+app.listen(4321, () => {
+    console.log(`Example app listening on port 4321`)
+})
